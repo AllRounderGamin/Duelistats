21828b16e5c514297e40b36e5220e4b83f4a9560
AllRounderGamin <allroundergamin@gmail.com>
2025-03-24 06:29:12 +0000

    Update Season 2 Char Icons

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -86,9 +86,11 @@ export default function AboutPage() {
               <ChangelogItem date=", 2025">
                 <List size="xs">
                   <List.Item>
+                    {' '}
                     Minor changes to change branding to Duelistats from Stats of Eden
                   </List.Item>
                   <List.Item>Reorder Character Page to match game order</List.Item>
+                  <List.Item>Gave season 2 character icons white border</List.Item>
                 </List>
               </ChangelogItem>
               <ChangelogItem date="February 20th, 2025">
